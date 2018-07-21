5bf8001c92ad8137d38fd3d70660a04c999f73b8
d_jingle <d_jingle@163.com>
2018-07-21 09:21:28 +0800

    jquery 1-16

---
+++ jquery-text/test.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "http://222.27.227.102:8080/bxwl/pic",
+        type: "get",
+        dataType:"json",
+        contentType : 'application/json',
+        success:function (result) {
+            if (result.code==0) {
+                for(var i=0;i<result.data.length;i++){
+                    if(i==0)
+                    {
+                        $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="'+i+'" class="active"></li>');
+                        $(".carousel-inner").append('<div class="item active"><img src="'+result.data[i].url+'"></div>');
+                    }
+                    else{
+                        $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="'+i+'"></li>');
+                        $(".carousel-inner").append('<div class="item"><img src="'+result.data[i].url+'"></div>');
+                    }
+
+                }
+
+            }
+            if(result.code==1){
+                console.log('1');
+            }
+            if(result.code==2){
+                console.log('2');
+            }
+            console.log(result.data);
+        },
+        error:function(){
+            alert('fail');
+        }
+    });
+    console.log('aaa');
+});
